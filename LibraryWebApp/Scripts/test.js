3ae3b762362fb5287ff4f07ee6d7769541fa0697
giancarlorhodes <giancarlo.rhodes@gmail.com>
2021-04-26 09:01:48 -0500

    playing with glyphicons

---
+++ LibraryWebApp/Scripts/test.js
@@ -0,0 +1,13 @@
+﻿
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+
+
+function myFunction(p1, p2) {
+    return p1 * p2;   // The function returns the product of p1 and p2
+}
+
+
